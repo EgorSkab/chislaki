2da9ee02595aeda39f0fab3abc55a2ea7430da62
EgorSkab <130250057+EgorSkab@users.noreply.github.com>
2023-09-23 00:17:36 +0300

    Add files via upload

---
+++ chisla 1/chisla1.1.cpp
@@ -0,0 +1,26 @@
+﻿#include "Header.h"
+
+int main() {
+	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+	vector<vector<float>> matrixA;
+	vector<float> matrixB;
+	int matrixSize;
+
+	cout << u8"Введите размерность матрицы: ";
+	cin >> matrixSize;
+
+	resizeMatrix(matrixA, matrixSize);
+	resizeMatrix(matrixB, matrixSize);
+
+	cout << u8"Введите матрицу коэффициентов:" << endl;
+	fillMatrix(matrixA);
+	cout << u8"Введите дополнение матрицы (ответы):" << endl;
+	fillMatrix(matrixB);
+
+	vector<float> solution = calculate(matrixA, matrixB);
+	for (int i = 0; i < solution.size(); i++) {
+		cout << setw(11) << solution[i];
+	}
+	//printMatrix(matrixA);
+}
