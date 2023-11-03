cefe99ac49493b7c80f9bc67cb66c55634132196
EgorSkab <130250057+EgorSkab@users.noreply.github.com>
2023-11-03 09:34:14 +0300

    Update simpson.h
    исправлена ошибка в строках 4 и 6

--- chisla 5/simpson.h
+++ chisla 5/simpson.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <cmath>
 
-double simpson_method(double(&function)(double), const double& A, const double& B, const unsigned long& m);
+double simpson_method(double(&function)(const double&), const double& A, const double& B, const unsigned long& m);
 
-double calculate_simpson(double(&function)(double), const double& A, const double& B, const double& epsilon);
+double calculate_simpson(double(&function)(const double&), const double& A, const double& B, const double& epsilon);
