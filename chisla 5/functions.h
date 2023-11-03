2d7c35864a26a5363f7b261c54b04f2377b0597b
EgorSkab <130250057+EgorSkab@users.noreply.github.com>
2023-11-03 09:26:19 +0300

    Update functions.h
    исправлены типы данных, передаваемых в функции

--- chisla 5/functions.h
+++ chisla 5/functions.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <cmath>
 
-double function1(double x);
-double function2(double x);
-double function3(double x);
-double function4(double x);
-double function5(double x);
-double function6(double x);
-double function7(double x);
-double function8(double x);
+double function1(const double& x);
+double function2(const double& x);
+double function3(const double& x);
+double function4(const double& x);
+double function5(const double& x);
+double function6(const double& x);
+double function7(const double& x);
+double function8(const double& x);
