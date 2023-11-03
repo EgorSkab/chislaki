ed9e5902db2365e81be51d6e460fec702d144bfb
EgorSkab <130250057+EgorSkab@users.noreply.github.com>
2023-11-03 09:33:09 +0300

    Update trapezoid.h
    исправлена ошибка в строках 4 и 6

--- chisla 5/trapezoid.h
+++ chisla 5/trapezoid.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <cmath>
 
-double trapezoid_method(double(&function)(double), const double& A, const double& B, const unsigned long& n);
+double trapezoid_method(double(&function)(const double&), const double& A, const double& B, const unsigned long& n);
 
-double calculate_trapezoid(double(&function)(double), const double& A, const double& B, const double& epsilon);
+double calculate_trapezoid(double(&function)(const double&), const double& A, const double& B, const double& epsilon);
