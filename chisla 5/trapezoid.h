6fad8ae22428b718c76f1994aa5866c7ccc4b279
EgorSkab <130250057+EgorSkab@users.noreply.github.com>
2023-11-03 09:25:51 +0300

    Update trapezoid.h
    исправлены типы данных, передаваемых в функции

--- chisla 5/trapezoid.h
+++ chisla 5/trapezoid.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <cmath>
 
-double trapezoid_method(double(&function)(double), const double& A, const double& B, unsigned long n);
+double trapezoid_method(double(&function)(double), const double& A, const double& B, const unsigned long& n);
 
-double calculate_trapezoid(double(&function)(double), const double& A, const double& B, double epsilon);
+double calculate_trapezoid(double(&function)(double), const double& A, const double& B, const double& epsilon);
