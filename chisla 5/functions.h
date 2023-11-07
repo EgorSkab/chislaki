438d9ab10a05e66591efce7cc8a867a4530320e4
EgorSkab <130250057+EgorSkab@users.noreply.github.com>
2023-11-07 21:32:19 +0300

    Update functions.h
    добавлены функции для вычисления двойного интеграла через кубатурную функцию Симпсона

--- chisla 5/functions.h
+++ chisla 5/functions.h
@@ -9,3 +9,9 @@ double function5(const double& x);
 double function6(const double& x);
 double function7(const double& x);
 double function8(const double& x);
+
+double function29(const double& x, const double& y);
+double function30(const double& x, const double& y);
+double function31(const double& x, const double& y);
+double function32(const double& x, const double& y);
+double function33(const double& x, const double& y);
