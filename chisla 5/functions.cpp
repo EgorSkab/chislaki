e92e1b92e06923a2202293d6c483546ea71c2c1e
EgorSkab <130250057+EgorSkab@users.noreply.github.com>
2023-11-09 20:25:58 +0300

    Update functions.cpp
    Исправлено условие функции 5 варианта

--- chisla 5/functions.cpp
+++ chisla 5/functions.cpp
@@ -4,7 +4,7 @@ double function1(const double& x) { return sqrt(1 + pow(x, 3)); }
 double function2(const double& x) { return 1 / (pow(log10(x), 2) + 1); }
 double function3(const double& x) { return sqrt(x + pow(x, 3)); }
 double function4(const double& x) { return (1 + pow(x, 2)) / (1 + pow(x, 3)); }
-double function5(const double& x) { return sqrt(pow(sin(x), 2) / (1 + pow(x, 3))); }
+double function5(const double& x) { return pow(sin(x), 2) / sqrt((1 + pow(x, 3))); }
 double function6(const double& x) { return exp(x / 2) / sqrt(x + 1); }
 double function7(const double& x) { return sqrt(1 + 2 * pow(x, 3)); }
 double function8(const double& x) { return 1 / (pow(log10(x), 2) + 1); }
