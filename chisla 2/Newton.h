525246b67c284484df1d036338efa0294195ddf4
EgorSkab <130250057+EgorSkab@users.noreply.github.com>
2023-12-15 23:37:34 +0300

    Update Newton.h
    исправлена проблема с кодировкой файла

--- chisla 2/Newton.h
+++ chisla 2/Newton.h
@@ -5,11 +5,11 @@
 #include <vector>
 using namespace std;
 
-// Функции, для которых мы решаем систему уравнений
+// Р¤СѓРЅРєС†РёРё, РґР»СЏ РєРѕС‚РѕСЂС‹С… РјС‹ СЂРµС€Р°РµРј СЃРёСЃС‚РµРјСѓ СѓСЂР°РІРЅРµРЅРёР№
 double f1(vector<double> solution);
 double f2(vector<double> solution);
 
-// Частные производные для исходных функций
+// Р§Р°СЃС‚РЅС‹Рµ РїСЂРѕРёР·РІРѕРґРЅС‹Рµ РґР»СЏ РёСЃС…РѕРґРЅС‹С… С„СѓРЅРєС†РёР№
 double df1_dx1(vector<double> solution);
 double df2_dx1(vector<double> solution);
 double df1_dx2(vector<double> solution);
